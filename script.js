2cbe5f69a0aa81a2f021fd1e4e274572921ca154
Zoe SMITH <zoe.smith@student.ecc.sa.edu.au>
2022-09-21 10:33:21 +0930

    add pieces

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//$(document).ready(function(){
+$(document).ready(function(){
 
     class square{
         constructor(number, piece, colour) {
@@ -10,7 +10,7 @@
     }       
     let row1=[];
     let row2=[];
-    let row3=[];
+    let row3=[]; 
     let row4=[];
     let row5=[];
     let row6=[];
@@ -18,6 +18,8 @@
     let row8=[];
     
     let board = [row1, row2, row3, row4, row5, row6, row7, row8];
+    let whitePieces = [wRook1, wKnight1, wBishop1, wKing, wQueen, wBishop2, wKnight2, wRook2, wPawn1, wPawn2, wPawn3, wPawn4, wPawn5, wPawn6, wPawn7, wPawn8];
+    let blackPieces = [bRook1, bKnight1, bBishop1, bKing, bQueen, bBishop2, bKnight2, bRook2, bPawn1, bPawn2, bPawn3, bPawn4, bPawn5, bPawn6, bPawn7, bPawn8];
 
 
     for(i=0; i<board.length; i++){
@@ -34,5 +36,6 @@
     }
 
     console.log(board);
+    initialiseBoard()
 
-//});
+});
